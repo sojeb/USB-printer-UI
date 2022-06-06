6607496a52171d3f45b00af4b55e6e3055baba8a
mdsohiduddin <mdsohiduddin@usbd.com.bd>
2022-06-06 18:57:39 +0600

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace UsbaPrinter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.A4sizePrinter = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TharmalPrinter = new System.Windows.Forms.ComboBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(277, -18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(193, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Printer From here ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(42, 187);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 41);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Printer 1 (A4 Size) : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // A4sizePrinter
+            // 
+            this.A4sizePrinter.FormattingEnabled = true;
+            this.A4sizePrinter.Location = new System.Drawing.Point(239, 198);
+            this.A4sizePrinter.Margin = new System.Windows.Forms.Padding(4);
+            this.A4sizePrinter.Name = "A4sizePrinter";
+            this.A4sizePrinter.Size = new System.Drawing.Size(342, 24);
+            this.A4sizePrinter.TabIndex = 6;
+            this.A4sizePrinter.SelectedValueChanged += new System.EventHandler(this.A4sizePrinter_SelectedValueChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(42, 262);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Printer 2 (Tharmel) : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TharmalPrinter
+            // 
+            this.TharmalPrinter.FormattingEnabled = true;
+            this.TharmalPrinter.Location = new System.Drawing.Point(239, 266);
+            this.TharmalPrinter.Margin = new System.Windows.Forms.Padding(4);
+            this.TharmalPrinter.Name = "TharmalPrinter";
+            this.TharmalPrinter.Size = new System.Drawing.Size(342, 24);
+            this.TharmalPrinter.TabIndex = 9;
+            this.TharmalPrinter.SelectedValueChanged += new System.EventHandler(this.TharmalPrinter_SelectedValueChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Crimson;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(239, 332);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(159, 35);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "Stop Service";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(434, 332);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(147, 35);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Start Service ";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(692, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 32);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "X";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label5.Location = new System.Drawing.Point(42, 124);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(212, 41);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Listening Url: ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(239, 135);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(342, 22);
+            this.urlText.TabIndex = 14;
+            this.urlText.Text = "http://localhost:8000/";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(604, 304);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(726, 394);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.TharmalPrinter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.A4sizePrinter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox A4sizePrinter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox TharmalPrinter;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsbaPrinter
+{
+    public partial class Form1 : Form
+    {
+        static String path = "";
+        static String serviceName = "PrintingService";
+        private ServiceProcessInstaller process;
+        private ServiceInstaller serviceInstaller1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            path = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\service" + "\\PrintingService.exe";
+        }
+
+        String folderDirectory = "C:\\Users\\Public\\Documents" + "\\printer";
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string selected = this.A4sizePrinter.GetItemText(this.A4sizePrinter.SelectedItem);
+            string selected2 = this.TharmalPrinter.GetItemText(this.TharmalPrinter.SelectedItem);
+
+
+            string[] printer = { selected, selected2 };
+            string output = string.Join("\n", printer);
+            try
+            {
+
+                String FileName = folderDirectory + "\\printerdata.txt";
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                    saveInfo(FileName, output);
+                }
+                else
+                {
+                    saveInfo(FileName, output);
+                }
+
+                String urlFileName = folderDirectory + "\\url.txt";
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                    saveInfo(urlFileName, urlText.Text);
+                }
+                else
+                {
+                    saveInfo(urlFileName, urlText.Text);
+                }
+
+
+                InstallService(serviceName, Assembly.LoadFrom(path));
+
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void InstallService(string serviceName, Assembly assembly)
+        {
+
+
+            using (AssemblyInstaller installer = GetInstaller(assembly))
+            {
+                //GetServiceAccount(installer)
+                IDictionary state = new Hashtable();
+                try
+                {
+                    installer.Install(state);
+                    installer.Commit(state);
+                    MessageBox.Show("Service started Successfully", "Printer Data ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch
+                {
+                    try
+                    {
+                        installer.Rollback(state);
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                    throw;
+                }
+            }
+
+        }
+
+        public static void startService()
+        {
+
+            if (IsServiceInstalled(serviceName))
+            {
+                ServiceController sc = new ServiceController(serviceName);
+                if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    sc.Start();
+                }
+                return;
+            }
+        }
+
+        public static bool IsServiceInstalled(string serviceName)
+        {
+            using (ServiceController controller = new ServiceController(serviceName))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        private static AssemblyInstaller GetInstaller(Assembly assembly)
+        {
+            AssemblyInstaller installer = new AssemblyInstaller(assembly, null);
+            installer.UseNewContext = true;
+
+            return installer;
+        }
+
+        public void saveInfo(String path, String output) {
+            try
+            {
+                using (StreamWriter sw = File.CreateText(path)) { sw.WriteLine(output); }
+                
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrinterList();
+           
+        }
+
+        private void PrinterList()
+        {
+             String FileName = folderDirectory + "/printerdata.txt";
+            String printer1="", printer2="";
+
+            if (File.Exists(FileName))
+            {
+                string[] lines = File.ReadAllLines(FileName);
+
+                printer1 = lines[0].Trim();
+                printer2 = lines[1].Trim();
+                btnSave.Enabled = true;
+            }
+            else {
+                btnSave.Enabled = false;
+            }
+
+            foreach (string sPrinters in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                A4sizePrinter.Items.Add(sPrinters);
+                TharmalPrinter.Items.Add(sPrinters);
+
+
+            }
+
+
+            A4sizePrinter.SelectedIndex = A4sizePrinter.FindStringExact(printer1);
+            TharmalPrinter.SelectedIndex = A4sizePrinter.FindStringExact(printer2);
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+
+            removeService();
+
+
+            Application.Exit();
+        }
+        public async Task callUninstallerAsync() {
+            await Task.Run(() => removeService());
+        }
+
+        private void PrinterSelect_Load(object sender, EventArgs e)
+        {
+            PrinterList();
+
+            
+
+        }
+
+       
+
+        private void A4sizePrinter_SelectedValueChanged_1(object sender, EventArgs e)
+        {
+            if (A4sizePrinter.Items == null)
+            {
+                btnSave.Enabled = false;
+            }
+            else {
+                btnSave.Enabled = true;
+            }
+        }
+
+        private void TharmalPrinter_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (TharmalPrinter.Items == null)
+            {
+                btnSave.Enabled = false;
+            }
+            else {
+                btnSave.Enabled = true;
+            }
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        public void removeService()
+        {
+            try
+            {
+                ServiceInstaller ServiceInstallerObj = new ServiceInstaller();
+                InstallContext Context = new InstallContext("<<log file path>>", null);
+                ServiceInstallerObj.Context = Context;
+                ServiceInstallerObj.ServiceName = serviceName;
+                ServiceInstallerObj.Uninstall(null);
+            }
+            catch (Exception e) { 
+            }
+
+          
+
+        }
+    }
+}
